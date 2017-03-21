d25192de0db77f924803771569057cd0ca26ab92
Palaskan <onecarlostlol@gmail.com>
2017-03-21 13:19:13 +0100

    Cambiado lo de los idiomas a un menu con enlaces a los idiomas
    Signed-off-by: Palaskan <onecarlostlol@gmail.com>

---
+++ sdi2-56/src/uo/sdi/presentation/BeanSettings.java
@@ -0,0 +1,53 @@
+package uo.sdi.presentation;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.faces.bean.*;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+
+@ManagedBean(name = "settings")
+@SessionScoped
+public class BeanSettings implements Serializable {
+	private static final long serialVersionUID = 2L;
+	private static final Locale ENGLISH = new Locale("en");
+	private static final Locale SPANISH = new Locale("es");
+	private Locale locale = new Locale("es");
+
+	@PostConstruct
+	public void init() {
+		System.out.println("BeanSettings - PostConstruct");
+	}
+
+	// Es sólo a modo de traza.
+	@PreDestroy
+	public void end() {
+		System.out.println("BeanSettings - PreDestroy");
+	}
+
+	public Locale getLocale() {
+		return (locale);
+	}
+
+	public void setSpanish(ActionEvent event) {
+		locale = SPANISH;
+		try {
+			FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	public void setEnglish(ActionEvent event) {
+		locale = ENGLISH;
+		try {
+			FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+}
